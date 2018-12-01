456a8cd3b07abd1a40b06843e0c08641808da896
PDXCrow <piratehans@gmail.com>
2018-11-30 16:55:34 -0800

    GalleryView hooked up with data and indexing

--- src/components/albums/images/GalleryView.vue
+++ src/components/albums/images/GalleryView.vue
@@ -1,25 +1,41 @@
 <template>
-    <div>
-        <h3>I'm a GalleryView</h3>
-        <ul>
-            <li
-                v-for="(image, index) in images"
-                :key="index">
-                <h4>{{image.title}}</h4>
-                <p>
-                    <img :src="image.url">
-                </p>
-            </li>
-        </ul>
-    </div>
+<section>
+    <h3>Gallery View</h3>
+    {{selectedIndex}}
+    <p>
+        <button @click="setIndex(-1)">&lt;</button>
+            <img :src="image.url">
+        <button @click="setIndex(1)">&gt;</button>
+    </p>
+</section>
 </template>
 
 <script>
 export default {
+  data() {
+    return {
+      selectedIndex: 0
+    };
+  },  
   props: {
     images: Array 
+  },
+  computed: {
+    image() {
+      return this.images[this.selectedIndex];
+    }
+  },
+  methods: {
+    setIndex(amt) {
+      this.selectedIndex += amt;
+      if(this.selectedIndex === this.images.length) {
+        this.selectedIndex = 0;
+      }
+      else if(this.selectedIndex < 0) {
+        this.selectedIndex = this.images.length - 1;
+      }
+    } 
   }
-
 };
 </script>
 
