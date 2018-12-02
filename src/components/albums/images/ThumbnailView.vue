30cd827bd85478f1ca991bb939fb817db85ffaa9
‘Michael <pickup.patterns@gmail.com>
2018-12-02 10:05:19 -0800

    initial thumbnail grid stylings

--- src/components/albums/images/ThumbnailView.vue
+++ src/components/albums/images/ThumbnailView.vue
@@ -24,9 +24,29 @@ export default {
 </script>
 
 <style>
-
 ul {
-    list-style-type: none;
+  display: grid;
+  grid-template-columns: repeat(auto-fit, minmax(450px, 1fr));
+  grid-gap: 10px;
+  grid-auto-rows: 400px;
+  list-style-type: none;
+  margin: .75vw;
+  padding: .95vw;
+  min-height: 0;
+  overflow:hidden;
+  
 }
+ul h4 {
+    background-color:#f7ffe0;
+    color:#9f7e69;
+    text-align: center;
+    padding:10px;
+}
+ul img { 
+  width: 100%; 
+  height: auto; 
+}
+  
+  
 
 </style>
